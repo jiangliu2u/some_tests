2d7753fc3b07bc1bb81702548b204dfc7716a08b
ranger <jiangliu2u@163.com>
2019-06-03 16:22:06 +0800

    11

--- neotest/main.ts
+++ neotest/main.ts
@@ -1,40 +1,36 @@
 import {privKey} from "./pri"
-const { default: Neon, api, wallet, sc, nep5 } = require("@cityofzion/neon-js");
 
-const sendingKey = privKey;
-const receivingAddress = "ASmpTmH6Wg5nwtxoCeXRS68ocqBJgk2rTQ";
-const contractScriptHash = "f0c1f15cb3b7ec9466e63aff988aab4083e80dd1";
-const numOfDecimals = 4;
-const amtToSend = 1;
-const network = "TestNet";
-const additionalInvocationGas = 0;
-const additionalIntents = [];
-const apiProvider = new api.neoscan.instance("TestNet");
+const {default: Neon, api, wallet, sc, nep5} = require("@cityofzion/neon-js");
 
-console.log("\n\n--- API Provider ---");
-console.log(apiProvider);
-const account = new wallet.Account(sendingKey);
+// console.log(config);
+for (let i = 0; i < 3; i++) {
 
-console.log("\n\n--- Sending Address ---");
-console.log(account);
-const generator = nep5.abi.transfer(contractScriptHash, account.address, receivingAddress, amtToSend)
-const builder = generator();
-const script = builder.str;
-const gas = additionalInvocationGas;
-const intent = additionalIntents;
-const config = {
-    api: apiProvider, // The API Provider that we rely on for balance and rpc information
-    account: account, // The sending Account
-    intents: intent, // Additional intents to move assets
-    script: script, // The Smart Contract invocation script
-    gas: gas // Additional GAS for invocation.
-};
-console.log(config)
-Neon.doInvoke(config)
-    .then(config => {
-        console.log("--- Response ---");
-        console.log(config.response);
-    })
-    .catch(config => {
-        console.log(config);
-    });
+    const sendingKey = privKey;
+    const receivingAddress = "ASmpTmH6Wg5nwtxoCeXRS68ocqBJgk2rTQ";
+    const contractScriptHash = "f0c1f15cb3b7ec9466e63aff988aab4083e80dd1";
+    const additionalInvocationGas = 0;
+    const additionalIntents = [];
+    const apiProvider = new api.neoscan.instance("TestNet");
+
+    console.log(apiProvider);
+    const account = new wallet.Account(sendingKey);
+
+    const generator = nep5.abi.transfer(contractScriptHash, account.address, receivingAddress, 0.0001);//数量最后会乘以Decimals 4位小数就会乘以10^4
+    const builder = generator();
+    const script = builder.str;
+    const config = {
+        api: apiProvider, // The API Provider that we rely on for balance and rpc information
+        account: account, // The sending Account
+        intents: additionalIntents, // Additional intents to move assets
+        script: script, // The Smart Contract invocation script
+        gas: additionalInvocationGas // Additional GAS for invocation.
+    };
+    Neon.doInvoke(config)
+        .then(config => {
+            console.log("--- Response ---");
+            console.log(config.response['txid:']);
+        })
+        .catch(config => {
+            console.log(config);
+        });
+}
--- neotest/test3.ts
+++ neotest/test3.ts
@@ -2,7 +2,7 @@ const {default: Neon, api, wallet, u,rpc} = require("@cityofzion/neon-js");
 const sb = Neon.create.scriptBuilder();
 import {privKey2} from "./pri";
 
-const sendingKey = privKey2;
+const sendingKey = "c56f4887fef5841c9466395c6da6f89ee10009d3e6b7605b5046a08b057457a5";
 const receivingAddress = "ASmpTmH6Wg5nwtxoCeXRS68ocqBJgk2rTQ";
 const contractScriptHash = "f0c1f15cb3b7ec9466e63aff988aab4083e80dd1";
 const numOfDecimals = 4;
@@ -10,7 +10,7 @@ const amtToSend = 1;
 const network = "TestNet";
 const additionalInvocationGas = 0;
 const additionalIntents = [];
-const apiProvider = new api.neoscan.instance("https://api.nel.group/api/testnet");
+const apiProvider = new api.neoscan.instance("http://localhost:20003/");
 
 const account = new wallet.Account(sendingKey);
 
@@ -18,9 +18,9 @@ const account = new wallet.Account(sendingKey);
 const config = {
     api: apiProvider,
     script: Neon.create.script({
-        scriptHash: 'f0c1f15cb3b7ec9466e63aff988aab4083e80dd1',
-        operation: 'tranferFrom',
-        args: [u.str2hexstring(account.address), u.str2hexstring("AGCZsNnK1XQCKLueMsrhJtB8xXDk53y7YB"), u.str2hexstring(account.address), 1234567]
+        scriptHash: 'c07c9f66157a6aaabaa517a2da415408443f7e24',
+        operation: 'name',
+        args: []
     }),
     address: account.address,
     account,
