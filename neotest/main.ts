c3c67f3e25928fca6483a97cd15ad7f3eb280435
ranger <jiangliu2u@163.com>
2019-07-19 17:21:02 +0800

    11

--- neotest/main.ts
+++ neotest/main.ts
@@ -2,12 +2,11 @@ import {privKey} from "./pri"
 
 const {default: Neon, api, wallet, sc, nep5} = require("@cityofzion/neon-js");
 
-// console.log(config);
-for (let i = 0; i < 1; i++) {
+
 
     const sendingKey = privKey;
     const receivingAddress = "ASmpTmH6Wg5nwtxoCeXRS68ocqBJgk2rTQ";
-    const contractScriptHash = "210d382618958e01db4b1fd48f5316d75dfb80a3";
+    const contractScriptHash = "e74c1bb22522f44819bd7bfd55a9045cb68b5488";
     const additionalInvocationGas = 0;
     const additionalIntents = [];
     const apiProvider = new api.neoscan.instance("TestNet");
@@ -15,7 +14,7 @@ for (let i = 0; i < 1; i++) {
     console.log(apiProvider);
     const account = new wallet.Account(sendingKey);
 
-    const generator = nep5.abi.transfer(contractScriptHash, account.address, receivingAddress, 23);//数量最后会乘以Decimals 4位小数就会乘以10^4
+    const generator = nep5.abi.transfer(contractScriptHash, account.address, receivingAddress, 230);//数量最后会乘以Decimals 4位小数就会乘以10^4
     const builder = generator();
     const script = builder.str;
     const config = {
@@ -33,4 +32,4 @@ for (let i = 0; i < 1; i++) {
         .catch(config => {
             console.log(config);
         });
-}
+
