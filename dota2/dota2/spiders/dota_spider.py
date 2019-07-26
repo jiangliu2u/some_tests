6fa9c964ee3e9cba9c88834c375197d741d4b58c
ranger <jiangliu2u@163.com>
2019-07-27 00:31:25 +0800

    解析技能

--- dota2/dota2/spiders/dota_spider.py
+++ dota2/dota2/spiders/dota_spider.py
@@ -3,28 +3,48 @@ import scrapy
 
 class Dota2(scrapy.Spider):
     name = "dota2"
-    url = "http://db.dota2.uuu9.com/hero/show/lc"
+    url = "http://db.dota2.uuu9.com/hero/show/SPE"
 
     def start_requests(self):
         yield scrapy.Request(self.url, self.parse)
 
     def parse(self, response):
+        # 每个等级的技能效果
         result = response.xpath(
             "/html/body/div[2]/div[1]/div[3]/div/div[2]/div/div/div[6]/div[2]/ul/li/div[2]/p/text()").extract()
-            "/html/body/div[2]/div[1]/div[3]/div/div[2]/div/div/div[6]/div[2]/ul/li/div[2]/p/span/text()").extract()
+        # 技能描述
+        result4 = response.xpath(
+            "/html/body/div[2]/div[1]/div[3]/div/div[2]/div/div/div[6]/div[2]/ul/li/div[2]/p/span[@class='red']/text()").extract()
         result2 = response.xpath(
             "/html/body/div[2]/div[1]/div[3]/div/div[2]/div/div/div[6]/div[2]/ul/li/div[2]/dl/text()").extract()
+        # 技能详情
         result3 = response.xpath(
-            "/html/body/div[2]/div[1]/div[3]/div/div[2]/div/div/div[6]/div[2]/ul/li/div[2]/dl/span/text()").extract()
-        result4 = response.xpath(
-        a = len(result)
-        print(len(result))
-        print(len(result2))
-        print(len(result3))
-        print(len(result4))
-        for i in range(0, a):
-            d = result[i]
-            e = result2[i]
-            f = result3[i]
-            g = result4[i]
-            print(d.strip())
+            "/html/body/div[2]/div[1]/div[3]/div/div[2]/div/div/div[6]/div[2]/ul/li/div[2]/p/span[@class='orange']/text()").extract()
+
+        # a = len(result)
+        # print(len(result))
+        # print(len(result2))
+        # print(len(result3))
+        # print(len(result4))
+        # result
+        temp1 = "/html/body/div[2]/div[1]/div[3]/div/div[2]/div/div/div[6]/div[2]/ul/li["
+        temp2 = "]/div[2]/p/span[@class='orange']/text()"#技能描述 施法距离 施法时间之类的前缀
+        temp4 = "]/div[2]/p/text()"#技能描述 施法距离 施法时间之类的具体值
+        temp6 = "]/div[1]/a[2]/text()"#技能名称
+        for i in range(0, len(result4)):
+            detail = result4[i]
+            temp3 = temp1+str(i+1)+temp2
+            temp5 = temp1+str(i+1)+temp4
+            temp7 = temp1+str(i+1)+temp6
+            hehe = response.xpath(temp3).extract()
+            xixi = response.xpath(temp5).extract()
+            skillname = response.xpath(temp7).extract()
+            a = []
+            print(skillname[0],detail)
+            for j in range(0, len(xixi)):
+                s = xixi[j].strip()
+                if len(s) > 0:
+                    a.append(s)
+            for j in range(0, len(a)):
+                print(hehe[j],a[j])
+            print("============")
